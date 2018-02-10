c2a3b83871920b45e4f2d10367f70513bdb74719
Mohit Gupta <mohitguptamt123@gmail.com>
2018-02-10 16:34:23 +0530

    server, gitignore added

---
+++ server.js
@@ -0,0 +1,15 @@
+var express    = require("express");
+ var mysql      = require('mysql');
+ var configDB = require('./config/database.js');
+ var connection = mysql.createConnection(configDB);
+ var app = express();
+ 
+ connection.connect(function(err){
+ if(!err) {
+     console.log("Database is connected ... \n\n");  
+ } else {
+     console.log("Error connecting database ... \n\n");  
+ }
+ });
+
+app.listen(3000);

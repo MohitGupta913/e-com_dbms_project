4451ea1dd36f61849709fab20fff972b068a3b23
rahulrawat1210 <rahulrawat1210@yahoo.co.in>
2018-02-10 17:26:44 +0530

    database.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express    = require("express");
  var app = express();
  
  connection.connect(function(err){
- if(!err) {
+ if(!!err) {
      console.log("Database is connected ... \n\n");  
  } else {
      console.log("Error connecting database ... \n\n");  

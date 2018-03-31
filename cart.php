b120cf9bf2ad72dc547f9b0eced14262d4fb27d0
rahulrawat1210@yahoo.co.in <rahulrawat1210@yahoo.co.in>
2018-03-31 18:17:54 +0530

    dead link fixed

--- cart.php
+++ cart.php
@@ -34,7 +34,7 @@ include("includes/db.php");
 				<li><a href="index.php">Home</a></li>
 				<li><a href="all_products.php">All Products</a></li>
 				<li><a href="customer/my_account.php">My Account</a></li>
-				<li><a href="#">Sign Up</a></li>
+				<li><a href="customer_register.php">Sign Up</a></li>
 				<li><a href="cart.php">Shopping Cart</a></li>
 				<li><a href="#">Contact Us</a></li>
 			

63b49ee946c89f6a952a49bdfe3a659904a79032
rahulrawat1210@yahoo.co.in <rahulrawat1210@yahoo.co.in>
2018-03-31 12:41:58 +0530

    delete images related to product and customer removed

--- admin_area/delete_c.php
+++ admin_area/delete_c.php
@@ -4,6 +4,17 @@
 	if(isset($_GET['delete_c'])){
 	
 	$delete_id = $_GET['delete_c'];
+
+	$cus_image = "select * from customers where customer_id='$delete_id'";
+	$run_cus_image = mysqli_query($con, $cus_image);
+	$cus_image_link = mysqli_fetch_array($run_cus_image);
+	$file = $cus_image_link['customer_image'];
+	$file_link = "../customer/customer_images/{$file}";
+
+
+	if( file_exists($file_link)){
+		unlink($file_link);
+	}
 	
 	$delete_c = "delete from customers where customer_id='$delete_id'"; 
 	

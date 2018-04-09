f2146b4a8d97f7acd989b7101501cf96649cf41f
Saif Haque <haque.saif1905@gmail.com>
2018-04-09 10:56:53 +0530

    details styling

--- details.php
+++ details.php
@@ -1,6 +1,6 @@
 <!DOCTYPE>
 <?php 
-
+session_start();
 include("functions/functions.php");
 
 ?>
@@ -9,10 +9,18 @@ include("functions/functions.php");
 		<title>My Online Shop</title>
 		
 		
-	<link rel="stylesheet" href="styles/style.css" media="all" /> 
+		<meta charset="utf-8"> 
+    <meta name="viewport" content="width=1000px, initial-scale=1">	 
+	<link rel="stylesheet" href="styles/style.css" media="all" />
+    <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css"/>
+    
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
 	</head>
 	
-<body>
+<body background="images/ecommerce2.jpg">
 	
 	<!--Main Container starts here-->
 	<div class="main_wrapper">
@@ -20,58 +28,125 @@ include("functions/functions.php");
 		<!--Header starts here-->
 		<div class="header_wrapper">
 		
-			<img id="logo" src="images/logo.gif" /> 
-			<img id="banner" src="images/ad_banner.gif" />
+		<a href="index.php"><img id="logo" src="images/logo_new.jpg" /> </a>
+			<img id="banner" src="images/jet-1.gif" />
 		</div>
 		<!--Header ends here-->
 		
 		<!--Navigation Bar starts-->
-		<div class="menubar">
-			
-			<ul id="menu">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">All Products</a></li>
-				<li><a href="#">My Account</a></li>
-				<li><a href="#">Sign Up</a></li>
-				<li><a href="#">Shopping Cart</a></li>
-				<li><a href="#">Contact Us</a></li>
-			
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <ul class="nav navbar-nav">
+			  <li><a href="index.php">Home</a></li>
+			  <li><a href="all_products.php">All Products</a></li>
+			  <li><a href="customer/my_account.php">My Account</a></li>
+	          <li><a href="customer_register.php">Sign Up</a></li>
+			  <li><a href="cart.php">Shopping Cart</a></li>
+			  <li><a href="#">Contact Us</a></li>
+
 			</ul>
-			
-			<div id="form">
-				<form method="get" action="results.php" enctype="multipart/form-data">
-					<input type="text" name="user_query" placeholder="Search a Product"/ > 
-					<input type="submit" name="search" value="Search" />
-				</form>
-			
-			</div>
-			
-		</div>
+			<form class="navbar-form navbar-left" action="results.php" enctype="multipart/form-data" method="get">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Search" name="user_query">
+                  <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit" name="search">
+                      <i class="glyphicon glyphicon-search"></i>
+                    </button>
+                  </div>
+              </div>
+            </form>
+
+		  
+		  </div>
+		</nav>
 		<!--Navigation Bar ends-->
 	
 		<!--Content wrapper starts-->
 		<div class="content_wrapper">
 		
-			<div id="sidebar">
+		<nav id="sidebar">
 			
-				<div id="sidebar_title">Categories</div>
+				<div class="sidebar-header"><h3>Categories</h3></div>
 				
-				<ul id="cats">
+				<ul class="list-unstyled components">
 				
 				<?php getCats(); ?>
 				
-				<ul>
+</ul>
 					
-				<div id="sidebar_title">Brands</div>
+				<div class="sidebar-header"><h3>Brands</h3></div>
 				
-				<ul id="cats">
+				<ul class="list-unstyled components">
 					
 					<?php getBrands(); ?>
 				
-				<ul>
+</ul>
+				<div class="sidebar-header"><h3>Filter</h3></div>
+				
+				<ul class="list-unstyled components">
+					
+					<form action='index.php' method='post' style="margin:0 auto;">
+					<table align='center'>
+						<tr>
+							<td><span style='color:white;'>Select Category</span></td>
+							<td align="right">
+								<select name='filter_cat' class="form-control">
+									<option value='0'>All</option>
+									
+									<?php
+										include('includes/db.php');
+										$get_cat = 'select * from categories';
+										$run_cat = mysqli_query($con, $get_cat); 
+										
+										while($row_cat=mysqli_fetch_array($run_cat)){
+											$cat_id = $row_cat['cat_id'];
+											$cat_title = $row_cat['cat_title'];
+											echo "
+												<option value='$cat_id'>$cat_title</option>
+											";
+											
+										}
+									?>
+								</select>
+						</tr><br>
+
+						<tr>
+							<td><span style='color:white;'>Select Brand</span></td>
+							<td align="right">
+								<select name='filter_brand' class="form-control">
+									<option value='0'>All</option>
+									
+									<?php
+										include('includes/db.php');
+										$get_brand = 'select * from brands';
+										$run_brand = mysqli_query($con, $get_brand); 
+										
+										while($row_brand=mysqli_fetch_array($run_brand)){
+											$brand_id = $row_brand['brand_id'];
+											$brand_title = $row_brand['brand_title'];
+											echo "
+												<option value='$brand_id'>$brand_title</option>
+											";
+											
+										}
+									?>
+								</select>
+						</tr>
+						<tr>
+							<td><span style='color:white;'>Order</span></td>
+							<td align="right"><select name='order' class="form-control">
+								<option value='1'>Price Low to High</option>
+								<option value='2'>Price High to Low</option>
+							</select></td>
+						</tr>
+						</table><br>
+						<input type='submit' name='filter' value='Filter'  class="btn btn-success" style="margin : 0 65px;">
+					</form>
+				
+									</ul>
 			
 			
-			</div>
+			</nav>
 		
 			<div id="content_area">
 			
@@ -79,14 +154,36 @@ include("functions/functions.php");
 					
 					<span style="float:right; font-size:18px; padding:5px; line-height:40px;">
 					
-					Welcome Guest! <b style="color:yellow">Shopping Cart -</b> Total Items: Total Price: <a href="cart.php" style="color:yellow">Go to Cart</a>
+					<?php 
+					if(isset($_SESSION['customer_email'])){
+					echo "<b>Welcome:</b>" . $_SESSION['customer_email'] . "<b style='color:yellow;'>Your</b>" ;
+					}
+					else {
+					echo "<b>Welcome Guest:</b>";
+					}
+					?>
+					
+					<b style="color:yellow">Shopping Cart -</b> Total Items: <?php total_items();?> Total Price: <?php total_price(); ?> <a href="cart.php" class="btn btn-info btn-lg" style="font-size:11px"><span class="glyphicon glyphicon-shopping-cart" style="font-size:10px"></span> Shopping Cart</a>
 					
 					
+					<?php 
+					if(!isset($_SESSION['customer_email'])){
+					
+					echo "<a href='checkout.php' ><button type='button' class='btn btn-info' >Login</button></a>";
+					
+					}
+					else {
+					echo "<a href='logout.php' ><button type='button' class='btn btn-info' >Logout</button></a>";
+					}
+					
+					
+					
+					?>
 					
 					</span>
 			</div>
 			
-				<div id="products_box">
+				<div id="products_box" style="margin-left: 70px; ">
 	<?php 
 	if(isset($_GET['pro_id'])){
 	
@@ -115,9 +212,9 @@ include("functions/functions.php");
 					
 					<p>$pro_desc </p>
 					
-					<a href='index.php' style='float:left;'>Go Back</a>
+					<a href='index.php' style='float:left;' style='float:left; color:black'>Details</a>
 					
-					<a href='index.php?pro_id=$pro_id'><button style='float:right'>Add to Cart</button></a>
+					<a href='index.php?pro_id=$pro_id'><button style='float:right' type='button' class='btn btn-success'>Add to Cart</button></a>
 				
 				</div>
 		
